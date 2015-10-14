44664ef0d049af1b5a5b5efa555499b40082f305
Kellen Kolbeck & Amit Mhatre <student@epicodus.com>
2015-10-14 15:45:00 -0700

    Add CRUD functionality to Song class

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,6 +14,29 @@ class SongsController < ApplicationController
     end
   end
 
+  def edit
+    @subgenres = Subgenre.find(params[:subgenre_id])
+    @songs = @subgenres.songs.find(params[:id])
+  end
+
+  def update
+    @subgenres = Subgenre.find(params[:subgenre_id])
+    @songs = @subgenres.songs.find(params[:id])
+    if @songs.update(song_params)
+      redirect_to subgenre_path(@subgenres)
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @subgenres = Subgenre.find(params[:subgenre_id])
+  end
+
+  def show
+    @subgenres = Subgenre.find(params[:subgenre_id])
+    @songs = @subgenres.songs.find(params[:id])
+  end
+
 
   private
   def song_params
